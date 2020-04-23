2fa05e5544b4b63bcac36fdd199f1b8bb520f041
Z4ty <Z4ty@users.noreply.github.com>
2020-04-23 19:31:45 +0800

    add js

--- js/sb-admin-2.js
+++ js/sb-admin-2.js
@@ -2,30 +2,48 @@
   "use strict"; // Start of use strict
 
   // Toggle the side navigation
-  $("#sidebarToggle, #sidebarToggleTop").on('click', function(e) {
-    $("body").toggleClass("sidebar-toggled");
-    $(".sidebar").toggleClass("toggled");
-    if ($(".sidebar").hasClass("toggled")) {
-      $('.sidebar .collapse').collapse('hide');
-    };
+  $('#sidebarCollapse').on('click', function () {
+      $('#sidebar').toggleClass('active');
+      $('#content').toggleClass('active');
+      $('.topnav').toggleClass('active');
   });
 
-  // Close any open menu accordions when window is resized below 768px
-  $(window).resize(function() {
-    if ($(window).width() < 768) {
-      $('.sidebar .collapse').collapse('hide');
-    };
+  // Toggle the side navigation
+  $('#close').on('click', function () {
+      $('#sidebar').toggleClass('active');
   });
 
-  // Prevent the content wrapper from scrolling when the fixed side navigation hovered over
-  $('body.fixed-nav .sidebar').on('mousewheel DOMMouseScroll wheel', function(e) {
-    if ($(window).width() > 768) {
-      var e0 = e.originalEvent,
-        delta = e0.wheelDelta || -e0.detail;
-      this.scrollTop += (delta < 0 ? 1 : -1) * 30;
-      e.preventDefault();
-    }
+  // Toggle the side navigation
+  $('.custom-menu').on('click', function () {
+      $('#sidebar').toggleClass('active');
+      $('#content').toggleClass('active');
+      $('.topnav').toggleClass('active');
+      $('.custom-menu').toggleClass('active');
+      // $('#product').remove(); /*remove option from sidebar*/
   });
+
+  //selected li
+    $("#sidebar ul li").on("click", function() {
+      $("#sidebar ul li").removeClass("active");
+      $(this).addClass("active");
+    });
+
+  // // Close any open menu accordions when window is resized below 768px
+  // $(window).resize(function() {
+  //   if ($(window).width() < 768) {
+  //     $('.sidebar .collapse').collapse('hide');
+  //   };
+  // });
+
+  // // Prevent the content wrapper from scrolling when the fixed side navigation hovered over
+  // $('body.fixed-nav .sidebar').on('mousewheel DOMMouseScroll wheel', function(e) {
+  //   if ($(window).width() > 768) {
+  //     var e0 = e.originalEvent,
+  //       delta = e0.wheelDelta || -e0.detail;
+  //     this.scrollTop += (delta < 0 ? 1 : -1) * 30;
+  //     e.preventDefault();
+  //   }
+  // });
 
   // Scroll to top button appear
   $(document).on('scroll', function() {
