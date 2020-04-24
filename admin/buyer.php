e20ec42d2c2fe5b08d6d304879704f7386426fc1
Z4ty <Z4ty@users.noreply.github.com>
2020-04-25 02:31:25 +0800

    buyer ui

--- admin/buyer.php
+++ admin/buyer.php
@@ -1,95 +1,91 @@
-
 <?php  include "../includes/admin_header.php"; ?>
 
+<div class="wrapper d-flex align-items-stretch">
 
-  <!-- Page Wrapper -->
-  <div id="wrapper">
-     
-        
-   <?php  include "includes/admin_sidebar_navigation.php"; ?>
+  <?php include "includes/admin_sidebar_navigation.php" ?>
+  
+  <!-- Page Content  -->
+  <div id="content" class="">
 
+    <?php include "../includes/topbar_nav.php" ?>
 
+    <div class="container-fluid">
+      
+    <div class="d-sm-flex align-items-center justify-content-between m-4">
+      <h1 class="h3 mb-0 text-gray-800">Pemborong</h1>
+    </div>
 
-    <!-- Content Wrapper -->
-    <div id="content-wrapper" class="d-flex flex-column">
+    <!-- Content Row -->
+    <div class="row">
 
-      <!-- Main Content -->
-      <div id="content">
+     <div class="col-xl-12">
+        <div class="card p-4 border">
+          <div class="card-title justify-content-between align-middle">
+            <i class="fas fa-angle-left fa-2x"></i>
+            <div id="search" class="form-group has-search">
+              <span class="fa fa-search form-control-feedback"></span>
+              <input type="text" class="form-control" placeholder="Search">
+           </div>
+          </div>
+            <div class="card-body">
+              <?php
 
-     
+                  if(isset($_GET['source'])){
+                      $source = $_GET['source'];
+                  } 
+                  else {
+                      $source = '';
+                  }
 
-          <!-- Top nav-->
-            <?php  include "includes/admin_topbar_navigation.php"; ?>
+                  switch($source) {
+                          
+                      case 'add_buyer';
+                          include "includes/add_buyer.php";
+                          break; 
 
-        
-           
+                      case 'edit_buyer';
+                          include "includes/edit_buyer.php";
+                          break;
+                      
+                      case 'view_buyer';
+                          include "includes/view_buyer.php";
+                          break; 
+                      
+                      case 'view_buyer_history';
+                          include "includes/view_buyer_history.php";
+                          break;
+                      
+                      case 'view_buyer_order_product';
+                          include "includes/view_buyer_order_product.php";
+                          break;
+                      
+                      case 'view_all_buyer_order_product';
+                          include "includes/view_all_buyer_order_product.php";
+                          break;
 
+                      case '200';
+                          echo "NICE 200";
+                          break;
 
+                      default:
+                          include "includes/view_all_buyers.php";
+                          break;
+                  }
 
-        <!-- Begin Page Content -->
-        <div class="container-fluid">
+              ?>              
+            </div>
+        </div>
+      </div>
 
-          
-        <?php
+    </div>
 
-            if(isset($_GET['source'])){
-                $source = $_GET['source'];
-            } 
-            else {
-                $source = '';
-            }
-
-            switch($source) {
-                    
-                case 'add_buyer';
-                    include "includes/add_buyer.php";
-                    break; 
-
-                case 'edit_buyer';
-                    include "includes/edit_buyer.php";
-                    break;
-                
-                case 'view_buyer';
-                    include "includes/view_buyer.php";
-                    break; 
-                
-                case 'view_buyer_history';
-                    include "includes/view_buyer_history.php";
-                    break;
-                
-                case 'view_buyer_order_product';
-                    include "includes/view_buyer_order_product.php";
-                    break;
-                
-                case 'view_all_buyer_order_product';
-                    include "includes/view_all_buyer_order_product.php";
-                    break;
-
-                case '200';
-                    echo "NICE 200";
-                    break;
-
-                default:
-                    include "includes/view_all_buyers.php";
-                    break;
-            }
-
-        ?>
 
  
-        </div>
-        <!-- /.container-fluid -->
 
-          
-          
-          
-          
-          
-      </div>
-      <!-- End of Main Content -->
-
+    </div>
+      
       <!-- Footer -->
-      <footer class="sticky-footer bg-white">
+      <footer class="sticky-footer">
         <div class="container my-auto">
           <div class="copyright text-center my-auto">
             <span>Copyright &copy; Agro Online System 2020 - Ver1.0</span>
@@ -98,35 +94,10 @@
       </footer>
       <!-- End of Footer -->
 
-    </div>
-    <!-- End of Content Wrapper -->
-
   </div>
-  <!-- End of Page Wrapper -->
 
-  <!-- Scroll to Top Button-->
-  <a class="scroll-to-top rounded" href="#page-top">
-    <i class="fas fa-angle-up"></i>
-  </a>
 
-  <!-- Logout Modal-->
-  <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
-          <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">×</span>
-          </button>
-        </div>
-        <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
-        <div class="modal-footer">
-          <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-          <a class="btn btn-primary" href="login.html">Logout</a>
-        </div>
-      </div>
-    </div>
-  </div>
+</div>
+
 
 <?php  include "../includes/admin_footer.php"; ?>
-
