ab855368e916da645ec87ca7dc085756346b8755
asyrafahmad <are.syraf9764@gmail.com>
2020-04-27 21:45:28 +0800

    create pages in table for admin

--- admin/includes/view_all_products.php
+++ admin/includes/view_all_products.php
@@ -5,10 +5,10 @@
 
     
 <!-- Page Heading -->
-          <p class="mb-4">Produk Pasaran. <a target="_blank" href="https://datatables.net">@PenerajuMedia.Sdn.Bhd</a>.</p>
+          <p class="mb-">Produk Pasaran. <a target="_blank" href="https://datatables.net">@PenerajuMedia.Sdn.Bhd</a>.</p>
 
           <!-- DataTales Example -->
-          <div class="card shadow mb-4">
+          <div class="card shadow mb-">
             <div class="card-header py-3">
               <h6 class="m-0 font-weight-bold text-primary">Senarai Produk</h6>
             </div>
@@ -17,6 +17,7 @@
 				  
 				  
 				<?php
+                  
 					if(isset($_GET['delete'])){
 
 						$product_id = $_GET['delete'];
@@ -26,10 +27,55 @@
 
 						echo "<p>Produk telah dibuang.</p>";
 					}
+                  
+                  
+                    if(isset($_GET['page'])){
 
+                        $page = $_GET['page'];
+                    }
+                    else{
+                        $page = "";
+                    }
+                  
+                    
+                    if($page == "" || $page == 1){
+                        $page_1 = 0;
+                    }
+                    else{
+                        $page_1 = ($page * 5) - 5;
+                    }
+                  
+
+                    $querys  =  "SELECT * FROM product ";    
+                    $find_count = mysqli_query($connection, $querys);
+                    $count = mysqli_num_rows($find_count);
+
+                    $count = $count/5;
 				?>    
                   
+                  <div  align="right">
+                   <div class="dataTables_paginate paging_simple_numbers" id="dataTable_paginate">
+                       <ul class="pagination">
+                           <li class="paginate_button page-item previous" id="dataTable_previous"><a class="page-link">Page</a></li>
+                           <?php
+                                for($i = 1; $i <= $count; $i++){
+                                    
+                                    if($i == $page){
+                                        echo "<li class='paginate_button page-item previous' id='dataTable_previous'><a href='product.php?page={$i}' aria-controls='dataTable' data-dt-idx='0' tabindex='0' class='page-link active_link'>{$i}</a> </li>";
+                                    }
+                                    else{
+                                        echo "<li class='paginate_button page-item previous' id='dataTable_previous'><a href='product.php?page={$i}' aria-controls='dataTable' data-dt-idx='0' tabindex='0' class='page-link'>{$i}</a> </li>";
+                                    }
+                                    
+                                    
+                                }
+                           ?>
+                           
+                        </ul>
+                  </div>
+                </div>
                   
+          
                   
                   
 <!--           TODO: put product table-->
@@ -54,10 +100,15 @@
                   <tbody>
                      <!-- Get data in db and display  -->
                     <?php
-					  
+                        
+                      
+                       
+                      
+                        // ----------------------------------------------------------------
+                      
 					  	$var = 0;
                       
-                        $query  =  "SELECT * FROM product ";    
+                        $query  =  "SELECT * FROM product LIMIT $page_1,5 ";    
                         $select_suppliers = mysqli_query($connection, $query);
 					  	
 
@@ -100,7 +151,6 @@
 
                        }
 					  
-					  echo $_SESSION['total'];
                   ?>
                   </tbody>
                 </table>
