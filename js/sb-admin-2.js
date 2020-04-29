c71450ba4ac571de16c42ae951c264e7491d1b45
Z4ty <Z4ty@users.noreply.github.com>
2020-04-30 01:58:47 +0800

    UI

--- js/sb-admin-2.js
+++ js/sb-admin-2.js
@@ -45,6 +45,21 @@
   //   }
   // });
 
+  function readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function(e) {
+            $('#imagePreview').css('background-image', 'url('+e.target.result +')');
+            $('#imagePreview').hide();
+            $('#imagePreview').fadeIn(650);
+        }
+        reader.readAsDataURL(input.files[0]);
+        }
+    }
+    $("#imageUpload").change(function() {
+        readURL(this);
+    });
+
   // Scroll to top button appear
   $(document).on('scroll', function() {
     var scrollDistance = $(this).scrollTop();
